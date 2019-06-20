c09e4585eb30074a08eb3455e9a7355c7f23ca32
Warrawit Patanavanich <44915469+SkipperXZ@users.noreply.github.com>
2019-06-20 15:50:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './Component/sidebar/sidebar.jsx';
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        <div style = {{margin : "10 px"}}>
+          dasdasdasdddddddddddddddddddddddddddddd
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
